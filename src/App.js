9180b99629281ea1165370b2264b12deebe0f7a9
priyankaTtk <120715630+priyankaTtk@users.noreply.github.com>
2024-09-12 11:17:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,25 @@
+import './App.css';
+import Navbar from './Navbar.jsx'
+import HeaderImage from './HeaderImage.jsx';
+import MoviesList from './MoviesList.jsx';
+import AgainList from './AgainList.jsx';
+import BottomNavbar  from './BottomNavbar.jsx';
+import Last from './Last.jsx';
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <HeaderImage/>
+      <MoviesList/>
+      <AgainList/>
+      <BottomNavbar/>
+      <Last/>
+
+    </div>
+  );
+}
+
+export default App;
 
